dcbd0ee7aed04d4c6deb8e1fcb80ea444b54c723
sodeidelphonse <sdidelphonse@gmail.com>
2024-04-05 04:20:58 +0100

    Code updated to include mesh building

--- geometry/DelaunayTriangulation.R
+++ geometry/DelaunayTriangulation.R
@@ -1,16 +1,17 @@
 
 #----------------------------------------------------
-#   Geometry for Spatial Analysis
+#   Geometry for Spatial Analysis and GIS Mapping
 #   By: Idelphonse SODE, Geospatial Data Scientist 
 #   University of Abomey-Calavi, Benin
 #----------------------------------------------------
 
 library(sp)
+library(rgeos)
 library(sf)
 library(ggplot2)
 
 
-## Delaunay Triangulation
+## Why use the Delaunay Triangulation ?
 
 # The Delaunay triangulation (DT) plays a major role in spatial statistics (e.g. Finite Element Method)
 # Its help define the mesh used in the FEM to find solution to Stochastic Partial Differential Equations 
@@ -45,4 +46,27 @@ ggplot(data = st_geometry(DT)) +
 ggsave("geometry/DelaunayTriangles.png", dpi = 300, width = 24, height = 28, units = "cm")
 
 
-## Exercise: Building mesh with INLA library
+## Exercise: Building a mesh with INLA library
+
+library(INLA)
+
+# Observed locations
+coo <- st_coordinates(meuse.sf)[, c("X","Y")]
+
+# Define the ConvexHull around the observed locations
+bnd  <- inla.nonconvex.hull(coo)  
+
+# Build the mesh (you can vary max.edge & offset and see the result)
+mesh <- inla.mesh.2d(loc = coo, boundary = bnd, offset = c(300, 600), 
+                     cutoff = 10,  max.edge = c(200, 300))      
+
+# Number of mesh nodes
+mesh$n
+
+# Save the mesh in the specified directory
+png(file = "geometry/mesh.png", width = 600, height = 600)
+plot(mesh)
+points(coo, col = "red", cex = 1)
+axis(1)
+axis(2)
+dev.off()
